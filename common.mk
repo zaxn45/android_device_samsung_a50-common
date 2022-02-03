4fe64281b6c5191fabbef03a701b4d30dc7460a5
santhosh6194 <santhosh6194@gmail.com>
2022-02-03 15:10:41 +0530

    a50-common: lineagify
    Signed-off-by: santhosh6194 <santhosh6194@gmail.com>

--- common.mk
+++ common.mk
@@ -246,6 +246,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.samsung
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.1-service.typec
