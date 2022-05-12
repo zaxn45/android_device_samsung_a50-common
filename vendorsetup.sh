fd09715631fe2d0d8f7293451c2df9cab82b387d
A50-Victor <80767544+victor4cris@users.noreply.github.com>
2022-05-12 09:51:25 +0300

    Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,19 @@
+### Start ###
+
+# Keystore patch
+echo "Applying Keystore patch...";
+cd system/security;
+git apply ../../device/samsung/a50-common/patch/Keystore.patch;
+cd - 
+
+# Light patch 
+cd frameworks/base && wget https://raw.githubusercontent.com/sarthakroy2002/random-stuff/main/Patches/Fix-brightness-slider-curve-for-some-devices-a12l.patch && patch -p1 < *.patch && cd -
+
+# Health fix
+
+sudo rm -r ./hardware/interfaces/health/2.1/default/android.hardware.health@2.1-service.rc
+cp ./device/samsung/a50-common/patch/android.hardware.health@2.1-service.rc ./hardware/interfaces/health/2.1/default/
+
+# BT calls fix 
+
+## Left to be filled
